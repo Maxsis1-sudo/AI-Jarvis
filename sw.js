6b9397b6fcbff65ebc639ec434b0b363a025cd18
Maxsis1-sudo <Maxsis1@seznam.cz>
2026-08-10 21:36:59 +0200

    Refresh PWA cache for speaker workflow

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='hopi-meeting-v1';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='hopi-meeting-v2';
+const ASSETS=['./','./index.html','./styles.css','./config.js','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',event=>{
