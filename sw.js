0f6204a648588f91c704bbaa0cadd3ccf666b38f
Maxsis1-sudo <Maxsis1@seznam.cz>
2026-08-11 21:11:19 +0200

    Cache production safeguards

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='hopi-meeting-production-v1';
+const CACHE='hopi-meeting-production-v2';
 const ASSETS=[
-  './','./index.html','./styles.css','./app.js','./api-mode.js',
+  './','./index.html','./styles.css','./app.js','./production.js','./api-mode.js',
   './manifest.webmanifest','./icon.svg'
 ];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
